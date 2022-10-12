2a4abf55436149ca8c4d3becf7afefbf1c87bf96
gtheaded <gaston.turner@gmail.com>
2022-10-12 13:33:20 -0300

    constantes arregladas

--- src/constantes.js
+++ src/constantes.js
@@ -1,11 +1,15 @@
-const BACK_URL = 'http://localhost:3001';
+//const BACK_URL = 'http://localhost:3001';
 //const BACK_URL = "https://backpf-production.up.railway.app";
-const FRONT_URL = "http://localhost:3000";
+//const FRONT_URL = "http://localhost:3000";
 //const FRONT_URL = "https://front-wheat-gamma.vercel.app";
 //const CORS_URL = "https://front-wheat-gamma.vercel.app"; //si usamos un wildcard ('*') deja de funcionar la autenticación via Google, pero la local sigue funcionando.
 //const CORS_URL = "*";
-const CORS_URL = "http://localhost:3000";
+//const CORS_URL = "http://localhost:3000";
 
+//MIO
+const BACK_URL = "https://backpf-production-c63d.up.railway.app";
+const CORS_URL = "https://frontpf-swart.vercel.app";
+const FRONT_URL = "https://frontpf-swart.vercel.app";
 
 const GOOGLE_CLIENT_ID = '222245387934-d4rnduqia7e5hubjf17cbj8aua1n6qk6.apps.googleusercontent.com';
 const GOOGLE_CLIENT_SECRET = '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';
