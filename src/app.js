b018433ca870162139204c6453af33bcf9016d99
gtheaded <gaston.turner@gmail.com>
2022-10-12 14:00:16 -0300

    cookie parser

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ server.name = "API";
 
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
-server.use(cookieParser());
+server.use(cookieParser('estoesunsecreto'));
 server.use(morgan("dev"));
 
 //////////PASSPORT ojo que hay un import de cors mas arriba
